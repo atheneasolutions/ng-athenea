2be1b167dc7e8c0459a60eaef82e77608c70ec3c
Jhonatan Iberico <jhonatan.iberico@atheneasolutions>
2024-10-14 10:59:27 +0200

    breaking: path must be passed as option now

--- projects/utils/typescript-utils/src/lib/MockUtils.ts
+++ projects/utils/typescript-utils/src/lib/MockUtils.ts
@@ -1,3 +1,8 @@
+export interface GetMockOptions {
+  path: string,
+  delay: number,
+  default_value: any
+}
 
 /**
  * Function to get data from a mock file
@@ -7,18 +12,20 @@
  * @param {number} [delay=500] Delay on getting mock service in miliseconds
  * @return {*}  {Promise<any>}
  */
-export function getMock(path:string, delay: number = 500): Promise<any> {
+export function getMock(options:GetMockOptions): Promise<any> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        fetch(path)
+        fetch(options.path)
           .then(response => {
             if (!response.ok) {
-              throw new Error('Network response was not ok');
+              let style = `font-weight: bold; background-color: #FD8C73; color:white; padding: 3px 7px 3px 7px ; border-radius: 3px 3px 3px 3px;`;
+                console.error(`%cMockUtils`, style, "No mock file found, returning default value:", options.default_value);
+              return options.default_value;
             }
             return response.json();
           })
           .then(data => resolve(data))
           .catch(error => reject(error));
-      }, delay);
+      }, options.delay);
     });
   }
