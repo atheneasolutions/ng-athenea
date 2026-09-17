06fcd0ad3f91446429360a2e9f184f2e5d9dc858
marmelichathenea <mar.melich@atheneasolutions.com>
2026-09-17 17:03:17 +0200

    feat(atheneaform): afegir botó help al component bloodpreassure

--- projects/playground/src/main.ts
+++ projects/playground/src/main.ts
@@ -2,14 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { addIcons } from 'ionicons';
-import { chevronUpOutline, chevronDownOutline } from 'ionicons/icons';
+import { chevronUpOutline, chevronDownOutline, informationCircleOutline } from 'ionicons/icons';
 import { register } from 'swiper/element/bundle';
 
 register();
 
 addIcons({
   'chevron-up-outline': chevronUpOutline,
-  'chevron-down-outline': chevronDownOutline
+  'chevron-down-outline': chevronDownOutline,
+  'information-circle-outline': informationCircleOutline
 });
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
